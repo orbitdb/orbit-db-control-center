5def433649b3f6b5acbc8856042a4f2b88092cac
Mark Henderson <mark@mrh.io>
2021-05-31 20:40:12 -0400

    Adding comment, mostly to trigger CI

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { HashRouter as Router } from 'react-router-dom'
 
+// TODO: Fix IPFS initialization
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Router><App /></Router>, div)
